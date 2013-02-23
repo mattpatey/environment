6bbf3e611e03aaa5a1b993c1333090463a014c15
Matt Patey <matt.patey@gmail.com>
2013-02-23 17:30:37 +0100

    Cleaning-up Emacs config.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,47 +1,39 @@
-;; Set custom site Elisp location
+(setq custom-file "~/.emacs.d/custom.el")
 (setq site-elisp "~/.emacs.d/elisp")
 (add-to-list 'load-path site-elisp)
-;; (setq color-themes "~/.emacs.d/elisp/themes")
-;; (add-to-list 'load-path color-themes)
-
-;; Keep Emacs custom stuff in its own file
-(setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
 
-;; Start the emacsclient server so other processes can interact with
-;; my current Emacs instance
-;;(server-start)
-
-;; Auto-save
-(defun auto-save-file-name-p
-    (filename)
-    (string-match "^#.*#$" (file-name-nondirectory filename)))
-(defvar autosave-dir "~/tmp/emacs_autosaves/")
-(defun make-auto-save-file-name ()
-    (concat autosave-dir
-        (if buffer-file-name
-            (concat "#" (file-name-nondirectory buffer-file-name) "#")
-            (expand-file-name (concat "#%" (buffer-name) "#")))))
-
-;; Miscellaneous default options
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(blink-cursor-mode -1)
+
 (column-number-mode t)
+
+(global-hl-line-mode -1)
+
+(put 'narrow-to-region 'disabled nil)
+(put 'set-goal-column 'disabled nil)
 (put 'upcase-region 'disabled nil)
+
+(setq auto-save-default nil)
+(setq confirm-kill-emacs 'y-or-n-p)
 (setq grep-find-command "find . -type f '!' -wholename '*/.svn/*' -print0 | xargs -0 -e grep -nH -e ")
 (setq use-file-dialog nil)
+
 (setq-default comint-prompt-read-only t)
 (setq-default indent-tabs-mode nil)
-;;(show-ws-toggle-show-tabs)
 (setq-default inhibit-startup-message t)
 (setq-default next-line-add-newlines nil)
 (setq-default require-final-newline t)
-;;(setq-default scroll-step 1)
 (setq-default truncate-lines t)
-(setq confirm-kill-emacs 'y-or-n-p)
+
 (show-paren-mode)
 
-;; Powerline
-(load-library "powerline")
+(when window-system
+  (set-fringe-mode '(0 . 0)))
+
+(when window-system
+  (load-library "powerline"))
 
 ;; Python
 (require 'python-mode)
@@ -53,92 +45,21 @@
     (cons '("python" . python-mode)
         interpreter-mode-alist))
 (add-hook 'python-mode-hook
-    '(lambda ()
-         ;;(flymake-mode t)
-         (yas/minor-mode t)
-         (setq yas/root-directory
-             (concatenate 'string site-elisp "/yasnippet-0.6.1c/snippets"))
-         (yas/load-directory yas/root-directory)))
+  '(lambda ()
+    (yas/minor-mode t)
+      (setq yas/root-directory
+        (concatenate 'string site-elisp "/yasnippet-0.6.1c/snippets"))
+        (yas/load-directory yas/root-directory)))
 
-;; Use pep8 to get PEP8 violations in a new buffer.
-(custom-set-variables
-  '(py-pychecker-command "pychecker")
-  '(py-pychecker-command-args (quote ("")))
-  '(python-check-command "pychecker"))
-
-;; Use pyflakes to highlight Python errors and warnings
-;;
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "pyflymake.py" (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
-;;(add-hook 'find-file-hook 'flymake-find-file-hook)
-
-
-;; Pymacs
-;;
-;; Requires the pymacs Python package.
-;;
-;; See http://pymacs.progiciels-bpi.ca/index.html
-;; (autoload 'pymacs-apply "pymacs")
-;; (autoload 'pymacs-call "pymacs")
-;; (autoload 'pymacs-eval "pymacs" nil t)
-;; (autoload 'pymacs-exec "pymacs" nil t)
-;; (autoload 'pymacs-load "pymacs" nil t)
-
-;; Pymacs 0.24b2 is hardcoded to use python2.5 if PYMACS_PYTHON is not
-;; set. The important bit here is to make sure pymacs is actually
-;; installed in whatever version of Python pymacs.el decides to use.
-;; (setenv "PYMACS_PYTHON" "/usr/bin/python")
-
-;; Ropemacs
-;;
-;; See https://bitbucket.org/agr/ropemacs
-;;
-;; Requires:
-;;
-;;   Rope (http://pypi.python.org/pypi/rope)
-;;   Pymacs (http://pymacs.progiciels-bpi.ca/index.html)
-;;   ropemode (https://bitbucket.org/agr/ropemode)
-;; (require 'pymacs)
-;; (pymacs-load "ropemacs" "rope-")
-;; (define-key global-map [(meta .)] 'rope-goto-definition)
-
-;; javascript
-;;(autoload 'js2-mode "js2" nil t)
-;;(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-;;(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+;; JavaScript
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-;; Markdown mode
+;; Markdown
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (setq auto-mode-alist
-    (cons '("\\.text" . markdown-mode)
-        auto-mode-alist))
-
-;; TODO: Fix nxhtml mode.
-;; nxhtml mode
-;;(load (format "%s/nxhtml/autostart.el" site-elisp))
-
-;; org mode
-(add-hook 'org-mode-hook
-    '(lambda()
-         (auto-fill-mode)
-         (flyspell-mode)
-         (rst-minor-mode)))
-
-;; Shell mode
-(add-hook 'shell-mode-hook
-    '(lambda()
-         (toggle-truncate-lines t)
-         (ansi-color-for-comint-mode-on)))
+  (cons '("\\.text" . markdown-mode)
+    auto-mode-alist))
 
 ;; ;; SuperCollider
 ;; (if (eq system-type 'darwin)
@@ -159,11 +80,10 @@
 ;; Yasnippet
 (load-library "yasnippet-0.6.1c/yasnippet")
 
-;; look and feel
-
 (require 'color-theme)
-(color-theme-initialize)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/elisp/themes/solarized")
+(color-theme-initialize)
+(load-theme 'solarized-light t)
 
 ;; Emacs frame is broken on initialization in Xmonad. Apparently one
 ;; must put all frame-specific configuration settings for Emacs in
@@ -171,33 +91,21 @@
 ;;
 ;; See
 ;; http://www.haskell.org/haskellwiki/Xmonad/Frequently_asked_questions#Emacs_mini-buffer_starts_at_wrong_size
-(tool-bar-mode -1)
-(unless (eq window-system 'x)
+(when window-system
   (set-frame-font "-*-inconsolata-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
   (add-to-list 'default-frame-alist
     '(font . "-*-inconsolata-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1"))
   (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
   (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
   (if (fboundp 'menu-bar-mode) (menu-bar-mode -1)))
-(if (eq window-system nil)
-  (progn
-    (color-theme-sitaramv-solaris))
-  (progn
-    (load-theme 'solarized-light t)
-    (load-library "powerline")
-    (global-hl-line-mode -1)
-    (blink-cursor-mode -1)))
 
-
-
-;; Key-bindings.
-(windmove-default-keybindings 'meta)
-(global-set-key (kbd "C-x C-m") 'execute-extended-command)
-(global-set-key (kbd "C-c C-m") 'execute-extended-command)
-(global-set-key (kbd "C-w") 'backward-kill-word)
-(global-set-key (kbd "C-x C-k") 'kill-region)
-(global-set-key (kbd "C-c C-k") 'kill-region)
-(global-set-key (kbd "C-c r") 'run-current-branch)
+;; Key-bindings
+;; (global-set-key (kbd "C-x C-m") 'execute-extended-command)
+;; (global-set-key (kbd "C-c C-m") 'execute-extended-command)
+;; (global-set-key (kbd "C-w") 'backward-kill-word)
+;; (global-set-key (kbd "C-x C-k") 'kill-region)
+;; (global-set-key (kbd "C-c C-k") 'kill-region)
+;; (windmove-default-keybindings 'meta)
 
 ;; Experimental functionality
 (defun jump-to-mark ()
@@ -207,20 +115,3 @@ argument."
   (interactive)
   (set-mark-command t))
 (global-set-key (kbd "M-`") 'jump-to-mark)
-
-(defun exchange-point-and-mark-no-activate ()
-  "Identical to \\[exchange-point-and-mark] but will not activate
-the region."
-  (interactive)
-  (exchange-point-and-mark)
-  (deactivate-mark nil))
-(define-key global-map
-    [remap exchange-point-and-mark]
-    'exchange-point-and-mark-no-activate)
-
-(put 'set-goal-column 'disabled nil)
-(put 'narrow-to-region 'disabled nil)
-(set-fringe-mode '(0 . 0))
-
-'(flymake-errline ((t (:underline "red"))))
-;;'(default ((t (:inherit nil :stipple nil :background "#2e3436" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :family "Inconsolata"))))
