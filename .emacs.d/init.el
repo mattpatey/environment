ee4b1beab68045bcb05059f8e7f0a5907266daff
Matt Patey <matt.patey@gmail.com>
2015-12-13 13:33:00 +0100

    Cleaned-up Emacs init.el Removed all third party .el files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,117 +1,91 @@
-(setq custom-file "~/.emacs.d/custom.el")
-(setq site-elisp "~/.emacs.d/elisp")
-(add-to-list 'load-path site-elisp)
-(load custom-file 'noerror)
-
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-(blink-cursor-mode -1)
-
-(column-number-mode t)
-
-(global-hl-line-mode -1)
-
-(put 'narrow-to-region 'disabled nil)
-(put 'set-goal-column 'disabled nil)
-(put 'upcase-region 'disabled nil)
-
-(setq auto-save-default nil)
-(setq confirm-kill-emacs 'y-or-n-p)
-(setq grep-find-command "find . -type f '!' -wholename '*/.svn/*' -print0 | xargs -0 -e grep -nH -e ")
-(setq use-file-dialog nil)
-
-(setq-default comint-prompt-read-only t)
-(setq-default indent-tabs-mode nil)
+;; Handle startup.
+;;
+(setq package-enable-at-startup nil)
+(package-initialize)
+(setq initial-scratch-message "")
+(setq initial-major-mode 'text-mode)
 (setq-default inhibit-startup-message t)
+
+;; Emacs packaging
+;;
+(add-to-list 'package-archives
+ '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+;;;; Install packages in the package-list list. WIP: not sure this is
+;;;; something I really want to do.
+;;
+;;(setq package-list '(markdown-mode))
+;;(unless package-archive-contents
+;;  (package-refresh-contents))
+;;(dolist (package package-list)
+;;  (unless (package-installed-p package)
+;;    (package-install package)))
+
+;; File handling.
+;;
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(setq auto-save-default nil)
 (setq-default next-line-add-newlines nil)
 (setq-default require-final-newline t)
-(setq-default truncate-lines t)
 
+;; Visuals.
+;;
+(blink-cursor-mode 0)
+(column-number-mode t)
+(setq-default truncate-lines t)
+(setq use-file-dialog nil)
 (show-paren-mode)
 
-(when window-system
-  (set-fringe-mode '(0 . 0)))
-
-(when window-system
-  (load-library "powerline"))
-
-;; Python
-(require 'python-mode)
-(autoload 'python-mode "python-mode" "Python editing mode." t)
-(setq auto-mode-alist
-    (cons '("\\.py$" . python-mode)
-        auto-mode-alist))
-(setq interpreter-mode-alist
-    (cons '("python" . python-mode)
-        interpreter-mode-alist))
-(add-hook 'python-mode-hook
-  '(lambda ()
-    (yas/minor-mode t)
-      (setq yas/root-directory
-        (concatenate 'string site-elisp "/yasnippet-0.6.1c/snippets"))
-        (yas/load-directory yas/root-directory)))
-
-;; JavaScript
-(autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
-;; Markdown
-(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
-(setq auto-mode-alist
-  (cons '("\\.text" . markdown-mode)
-    auto-mode-alist))
-
-;; ;; SuperCollider
-;; (if (eq system-type 'darwin)
-;;     (progn
-;;         (custom-set-variables
-;;             '(sclang-auto-scroll-post-buffer t)
-;;             '(sclang-eval-line-forward nil)
-;;             '(sclang-help-path
-;;                  (quote ("/Applications/SuperCollider/Help")))
-;;             '(sclang-runtime-directory "~/.sclang/"))
-;;         (add-to-list 'load-path
-;;             "~/.emacs.d/vendor/supercollider/el")
-;;         (setq path "/Applications/SuperCollider")
-;;         (setenv "PATH" path)
-;;         (push "/Applications/SuperCollider" exec-path)
-;;         (require 'sclang)))
-
-;; Yasnippet
-(load-library "yasnippet-0.6.1c/yasnippet")
-
-(require 'color-theme)
-;;(add-to-list 'custom-theme-load-path "~/.emacs.d/elisp/themes/solarized")
-;;(color-theme-initialize)
-;;(load-theme 'solarized-light t)
-
-;; Emacs frame is broken on initialization in Xmonad. Apparently one
-;; must put all frame-specific configuration settings for Emacs in
-;; one's .Xresources file to make things look right.
+;; Tabs and spaces.
 ;;
-;; See
-;; http://www.haskell.org/haskellwiki/Xmonad/Frequently_asked_questions#Emacs_mini-buffer_starts_at_wrong_size
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+;; Keep settings made in customize-mode in a separate file.
+;; See: http://emacsblog.org/2008/12/06/quick-tip-detaching-the-custom-file/
+;;
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+
+;; Misc.
+;;
+(setq confirm-kill-emacs 'y-or-n-p)
+(setq-default comint-prompt-read-only t)
+
+;; Emacs behaviour in a windowed environment.
+;;
 (when window-system
+  (set-fringe-mode '(0 . 0))
+  ;; Emacs frame is broken on initialization in Xmonad. Apparently one
+  ;; must put all frame-specific configuration settings for Emacs in
+  ;; one's .Xresources file to make things look right.
+  ;;
+  ;; See
+  ;; http://www.haskell.org/haskellwiki/Xmonad/Frequently_asked_questions#Emacs_mini-buffer_starts_at_wrong_size
   (set-frame-font "-*-inconsolata-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
   (add-to-list 'default-frame-alist
     '(font . "-*-inconsolata-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1"))
   (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
   (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-  (if (fboundp 'menu-bar-mode) (menu-bar-mode -1)))
+  (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+)
 
-;; Key-bindings
-;; (global-set-key (kbd "C-x C-m") 'execute-extended-command)
-;; (global-set-key (kbd "C-c C-m") 'execute-extended-command)
-;; (global-set-key (kbd "C-w") 'backward-kill-word)
-;; (global-set-key (kbd "C-x C-k") 'kill-region)
-;; (global-set-key (kbd "C-c C-k") 'kill-region)
-;; (windmove-default-keybindings 'meta)
+;; Bind file extensions to Emacs modes.
+;;
+;; (add-to-list 'auto-mode-alist
+;;   '("\\.md" . markdown-mode)
+;;)
 
-;; Experimental functionality
+;; Keybindings
+;;
+(global-set-key (kbd "M-`") 'jump-to-mark)
+
+;; Experimental.
+;;
 (defun jump-to-mark ()
   "Jumps to the local mark, respecting the `mark-ring' order.
 This is the same as using \\[set-mark-command] with the prefix
 argument."
   (interactive)
   (set-mark-command t))
-(global-set-key (kbd "M-`") 'jump-to-mark)
