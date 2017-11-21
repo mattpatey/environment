63d379ee3a28b4249952a32e67d50a275bb2e483
Matt Patey <matt.patey@gmail.com>
2017-11-21 11:07:35 +0100

    Update environment

--- dotfiles/.xmonad/xmonad.hs
+++ dotfiles/.xmonad/xmonad.hs
@@ -1,25 +1,62 @@
+import XMonad.Util.Dmenu
+import qualified XMonad.StackSet as W
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Layout
+import XMonad.Layout.IndependentScreens
+import XMonad.Layout.NoBorders
+import XMonad.Layout.ResizableTile
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.Themes
 import System.IO
 
+mlp_borderWidth = 1
+mlp_focusedBorderColor = "#333"
+mlp_layoutHook = smartBorders (avoidStruts $ (Tall 1 (3/100) (1/2)) ||| Full )
+mlp_manageHook = composeAll [
+                   className =? "XCalc" --> doFloat
+	             , className =? "Vlc" --> doFloat
+                 ]
+                 <+> manageDocks
+                 <+> manageHook defaultConfig
+mlp_modMask = mod4Mask
+mlp_normalBorderColor = "#555555"
+mlp_terminal = "urxvt"
+
+mlp_PP :: PP
+mlp_PP = defaultPP {
+    ppCurrent = wrap "<fc=#ff0000>[" "]</fc>"
+    , ppTitle = xmobarColor "green" "" . shorten 60
+    , ppVisible = wrap "(" ")"
+    }
+
+myKeys =
+    [   ((mlp_modMask, xK_v), spawn "amixer -c 0 set PCM 2dB-")
+      , ((mlp_modMask, xK_b), spawn "amixer -c 0 set PCM 2dB+")
+      , ((mlp_modMask, xK_e), spawn "/usr/bin/emacs")
+      , ((mlp_modMask, xK_f), spawn "/usr/bin/thunar")
+      , ((mlp_modMask, xK_g), spawn "/usr/bin/firefox")
+      , ((mlp_modMask, xK_l), spawn "/usr/bin/xscreensaver-command -lock")
+    ]
+    ++
+    [((m .|. mlp_modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e, xK_r] [1,0,2]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+
 main = do
-     xmproc <- spawnPipe "/usr/bin/xmobar"
-     xmonad $ defaultConfig
-         { borderWidth        = 2
-         , terminal           = "urxvt"
-         , normalBorderColor  = "#cccccc"
-         , focusedBorderColor = "#cd8b00"
-         , manageHook         = manageDocks <+> manageHook defaultConfig
-         , layoutHook         = avoidStruts  $  layoutHook defaultConfig
-         , logHook            = dynamicLogWithPP xmobarPP
-             { ppOutput       = hPutStrLn xmproc
-     	     , ppTitle        = xmobarColor "green" "" . shorten 50
-             }
-         , modMask            = mod4Mask
-         } `additionalKeys`
-         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-         , ((mod4Mask,               xK_p), spawn "dmenu_run -nb '#222222' -sb '#222222' -nf '#cccccc' -sf gold -fn Inconsolata-10:style=Bold")
-         ]
+    pipe <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
+    xmonad $ defaultConfig {
+        borderWidth = mlp_borderWidth
+        , focusedBorderColor = mlp_focusedBorderColor
+        , layoutHook = mlp_layoutHook
+        , logHook = dynamicLogWithPP $ mlp_PP { ppOutput = hPutStrLn pipe }
+        , manageHook = mlp_manageHook
+        , modMask = mlp_modMask
+        , normalBorderColor = mlp_normalBorderColor
+        , terminal = mlp_terminal
+        , workspaces = myWorkspaces
+        } `additionalKeys` myKeys
